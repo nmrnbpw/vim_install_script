eb3fc88b64db55e986a630f01ae78034c047d162
nmrnbpw <n/a>
2020-01-05 08:32:53 -0500

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CICA_VER=5.0.1
+
+
+sudo yum install unzip -y
+
+mkdir -p cica
+pushd cica
+curl -LO https://github.com/miiton/Cica/releases/download/v${CICA_VER}/Cica_v${CICA_VER}_with_emoji.zip
+unzip Cica_v${CICA_VER}_with_emoji.zip
+
+mkdir -p ${HOME}/.fonts
+mv *.ttf ${HOME}/.fonts/
+popd
+
+
+git clone https://github.com/Shougo/dein.vim ${HOME}/.cache/dein/repos/github.com/Shougo/dein.vim
+
+
+echo ********************************************************************************
+echo    * edit terminal font *
+echo ********************************************************************************
+
